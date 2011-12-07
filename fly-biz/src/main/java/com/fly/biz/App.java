db9cb186aecc1e490e3c5d6aade987a56d2cea8d
daniel.taobaowang <daniel.taobaowang@gmail.com>
2011-12-07 09:17:56 +0000

    1.构建项目FLY

---
+++ fly-biz/src/main/java/com/fly/biz/App.java
@@ -0,0 +1,13 @@
+package com.fly.biz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
