0afadd03543117e8da15ef304dd8dcf21af7b583
daniel.taobaowang <daniel.taobaowang@gmail.com>
2011-12-08 15:00:37 +0000

    日志搞定稍侯给日志包装下统一格式

--- fly-web/src/main/java/com/fly/test/HelloWorld.java
+++ fly-web/src/main/java/com/fly/test/HelloWorld.java
@@ -1,32 +1,17 @@
 package com.fly.test;
 
+import org.apache.log4j.Logger;
+
+
+
 public class HelloWorld {  
 	  
-    private String message;  
-    private String userName;  
+	private static Logger logger = Logger.getLogger("appenderDefalut");
   
-    public HelloWorld(){  
+    public static void main(String args[]){ 
+    	System.out.println("ok");
+    	logger.error("ok");
     }  
-  
-    public String execute() {  
-        setMessage("Hello " + getUserName());  
-        return "SUCCESS";  
-    }  
-  
-    public String getMessage() {  
-        return message;  
-    }  
-  
-    public void setMessage(String message) {  
-        this.message = message;  
-    }  
-  
-    public String getUserName() {  
-        return userName;  
-    }  
-  
-    public void setUserName(String userName) {  
-        this.userName = userName;  
-    }  
+
   
 }  
