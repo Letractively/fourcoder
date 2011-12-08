86f0eb5c00c9f8ab9dfb4856f3a63fef1e50b79b
daniel.taobaowang@gmail.com <daniel.taobaowang@gmail.com@e3e78a05-86df-37b3-8c68-ccc1b520330a>
2011-12-08 14:34:16 +0000

    数据源配置完毕，spring与ibatis整合完成

---
+++ trunk/fly-biz/src/main/java/com/fly/biz/common/model/user/User.java
@@ -0,0 +1,28 @@
+package com.fly.biz.common.model.user;
+
+public class User {
+
+	private String userName;
+	private long id;
+	private String password;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
