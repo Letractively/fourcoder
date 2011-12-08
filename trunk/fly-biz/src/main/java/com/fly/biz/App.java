aff67a5d0c798e2711294d492780ba6028858234
daniel.taobaowang@gmail.com <daniel.taobaowang@gmail.com@e3e78a05-86df-37b3-8c68-ccc1b520330a>
2011-12-08 15:00:37 +0000

    日志搞定稍侯给日志包装下统一格式

--- trunk/fly-biz/src/main/java/com/fly/biz/App.java
+++ trunk/fly-biz/src/main/java/com/fly/biz/App.java
@@ -1,13 +1,19 @@
 package com.fly.biz;
 
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+
+	
     public static void main( String[] args )
     {
+    	
         System.out.println( "Hello World!" );
     }
 }
