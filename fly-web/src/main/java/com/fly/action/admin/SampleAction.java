9efb82dadd191ac00d4f72fca72d77c4ee5c7c90
daniel.taobaowang <daniel.taobaowang@gmail.com>
2011-12-10 14:50:28 +0000

    MVC优化

---
+++ fly-web/src/main/java/com/fly/action/admin/SampleAction.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.fly.action.admin;
+
+/**
+ * @author hengheng
+ * 该包都放一些后台的跳转
+ *
+ */
+public class SampleAction {
+
+	public String execute() throws Exception{
+	
+			return "success";
+
+	}
+}
--- fly-web/src/main/java/com/fly/test/HelloWorld.java
+++ fly-web/src/main/java/com/fly/test/HelloWorld.java
@@ -6,12 +6,8 @@ import org.apache.log4j.Logger;
 
 public class HelloWorld {  
 	  
-	private static Logger logger = Logger.getLogger("appenderDefalut");
-  
-    public static void main(String args[]){ 
-    	System.out.println("ok");
-    	logger.error("ok");
-    }  
+	public String execute() throws Exception{
 
-  
+			return "SUCCESS";
+	}
 }  
