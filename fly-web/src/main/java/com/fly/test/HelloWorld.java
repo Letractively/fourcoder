f9c11b27c3c07fd4770b072a66929a99443e8e99
daniel.taobaowang <daniel.taobaowang@gmail.com>
2011-12-11 08:00:00 +0000

    服务和managers配置

--- fly-web/src/main/java/com/fly/test/HelloWorld.java
+++ fly-web/src/main/java/com/fly/test/HelloWorld.java
@@ -1,13 +1,33 @@
 package com.fly.test;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fly.biz.service.user.UserService;
 
 
+public class HelloWorld {
 
-public class HelloWorld {  
-	  
-	public String execute() throws Exception{
+	private Logger logger= Logger.getLogger(HelloWorld.class);
+
+
+	private UserService userService;
+
+
+	public void setUserService(UserService userService){
+		this.userService = userService;
+	}
+
+
+	public String execute(){
+
+			String message="ok?";
+
+			message=userService.toTestString();
+
+			logger.error(message);
 
 			return "SUCCESS";
 	}
+	
 }  
